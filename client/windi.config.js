c9cd0f9f317945b966b797f7a3882e7764957c79
Mathias <mathiasayivor5@gmail.com>
2022-08-05 15:13:14 +0000

    feat: added shortcuts and theme extensions for colours

--- client/windi.config.js
+++ client/windi.config.js
@@ -2,12 +2,20 @@ import { defineConfig } from 'windicss/helpers';
 
 export default defineConfig({
     safelist: ['top-2 right-2 bottom-2 left-2'],
+    theme: {
+        extend: {
+            colors: {
+                primary: '#541515'
+            }
+        }
+    },
     shortcuts: {
         btn: 'h-[40px] py-2 px-4 rounded-md flex justify-center items-center disabled:curser-not-allowed uppercase',
-        'btn-primary': 'bg-[#541515] text-white disabled:bg-[#5415157d]',
-        input: 'h-[40px] py-2 px-4 border outline-none appearance-none w-full text-sm leading-6 text-slate-900 placeholder-slate-400 rounded-md transition-all duration-200 disabled:curser-not-allowed',
-        'input-primary': 'focus:border-[#541515]',
-        'text-primary': 'text-[#541515]',
-        'bg-primary': 'bg-[#541515] text-white',
+        'btn-primary': 'bg-primary text-white disabled:bg-primary/50',
+        input: 'h-[40px] py-2 px-4 border-1 outline-none appearance-none w-full text-sm leading-6 text-slate-900 placeholder-slate-400 rounded-md transition-all duration-200 disabled:curser-not-allowed',
+        'input-primary': 'focus:border-primary',
+        'text-primary': 'text-primary',
+        'bg-primary': 'bg-primary text-white',
+        'row': 'w-full grid grid-cols-12 gap-1'
     },
 });
